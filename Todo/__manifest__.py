d9795ad1dbc0c5de51b9a1f07b559adbe38b8d7f
aashish-acharya <aashish.acharya006@gmail.com>
2022-09-18 14:14:15 +0545

    Odoo Todo Project

---
+++ Todo/__manifest__.py
@@ -0,0 +1,8 @@
+{
+	'name':'To-Do Application',
+	'description': 'Manage Tasks',
+	'author': 'Aashish',
+	'depends':['base'],
+	'application':True,
+	'data':['security/ir.model.access.csv','security/todo_access_rules.xml','views/todo_menu.xml','views/todo_view.xml'],
+}
